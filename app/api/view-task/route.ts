ebfacfaa31325d3858d46f97e8d3792e408d8c0a
Salvatore Polizzotti <salvatore.polizzotti@libero.it>
2024-05-10 15:42:09 +0200

    fix: dynamic rendering next.js

--- app/api/view-task/route.ts
+++ app/api/view-task/route.ts
@@ -1,6 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
 export async function GET(request: Request) {
   const tasks = await sql`SELECT * FROM Tasks;`;
   return NextResponse.json(tasks, { status: 200 });
