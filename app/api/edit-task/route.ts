8c46e323e5a69fb69333e01832361b32e61a312e
Salvatore Polizzotti <salvatore.polizzotti@libero.it>
2024-05-20 16:45:37 +0200

    handling CORS errors

--- app/api/edit-task/route.ts
+++ app/api/edit-task/route.ts
@@ -7,11 +7,29 @@ export async function GET(request: Request) {
 }
 
 export async function PUT(request: Request) {
+  if (request.method === "OPTIONS") {
+    // Gestione della richiesta di preflight
+    return new Response(null, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods":
+          "GET, OPTIONS, PATCH, DELETE, POST, PUT",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  }
+
   const { taskId, taskName } = await request.json();
   await sql`UPDATE Tasks SET task = ${taskName} WHERE id = ${taskId};`;
 
-  const tasks = await sql`SELECT * FROM Tasks;`;
-  return NextResponse.json(tasks, {
-    status: 200,
-  });
+  const updatedTasks = await sql`SELECT * FROM Tasks;`;
+
+  const headers = {
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+
+  return NextResponse.json(updatedTasks, { status: 200, headers });
 }
